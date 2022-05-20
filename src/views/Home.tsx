903d885404b0e3837e30a0bda36bcec4d240a52d
heptacode <admin@hyunwoo.kim>
2022-05-20 14:57:33 +0900

    Modify Home View

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,22 @@
-import { Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export function Home() {
+  const navigate = useNavigate();
   return (
-    <>
-      <Button>시작하기</Button>
-    </>
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Typography component="h1" variant="h4">
+        Trivia Quiz
+      </Typography>
+
+      <Button onClick={() => navigate('/quiz')}>시작하기</Button>
+    </Box>
   );
 }
