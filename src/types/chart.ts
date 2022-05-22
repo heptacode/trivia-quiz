bceeb72416e31c5f2e826d721028a4734e7892e0
heptacode <admin@hyunwoo.kim>
2022-05-22 10:56:41 +0900

    Add Types TSDoc

--- src/types/chart.ts
+++ src/types/chart.ts
@@ -1,13 +1,16 @@
-import { DatumId, PieArc } from '@nivo/pie';
+import { DatumId } from '@nivo/pie';
 
+/**
+ * Nivo 차트 데이터
+ *
+ * @member {DatumId} id: 고유값
+ * @member {boolean} label?: 레이블
+ * @member {number} value: 값(수치)
+ * @member {string} color?: 색상
+ */
 export interface ChartData {
   id: DatumId;
   label?: DatumId;
   value: number;
-  formattedValue?: string;
   color?: string;
-  fill?: string;
-  data?: any;
-  arc?: PieArc;
-  hidden?: boolean;
 }
--- src/types/quiz.ts
+++ src/types/quiz.ts
@@ -1,11 +1,27 @@
 import { ResponseCode } from './enums';
 import { QuizDifficulty, QuizType } from './types';
 
+/**
+ * 선택지
+ *
+ * @member {string} value: 선택항목
+ * @member {boolean} isAnswer: 정답여부
+ */
 export interface Choice {
   value: string;
   isAnswer: boolean;
 }
 
+/**
+ * 퀴즈
+ *
+ * @member {string} category: 범주
+ * @member {QuizType} type: 종류
+ * @member {QuizDifficulty} difficulty: 난이도
+ * @member {string} question: 질문
+ * @member {string} correct_answer: 정답
+ * @member {string[]} incorrect_answers: 오답 배열
+ */
 export interface Quiz {
   category: string;
   type: QuizType;
@@ -15,6 +31,12 @@ export interface Quiz {
   incorrect_answers: string[];
 }
 
+/**
+ * 퀴즈 API 응답 결과
+ *
+ * @member {ResponseCode} response_code: 응답 코드
+ * @member {Quiz[]} results: 결과
+ */
 export interface QuizResponse {
   response_code: ResponseCode;
   results: Quiz[];
