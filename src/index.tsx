f21915cd5e1abbc5092795a98cb77bbdb907a479
heptacode <admin@hyunwoo.kim>
2022-05-22 10:55:25 +0900

    Modify Index Styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import '@/index.css';
 import { Router } from '@/Router';
-import { AppBar, Container, Typography } from '@mui/material';
+import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppBar position="fixed">
-      <Typography variant="h5" noWrap>
-        &nbsp;
-      </Typography>
-    </AppBar>
-    <Container maxWidth="sm">
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Trivia Quiz
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    <Container component="main" maxWidth="sm">
       <Router />
     </Container>
   </React.StrictMode>
