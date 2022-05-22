1ab7123332adf8adc144b0d46b3ab15f0827edd9
heptacode <admin@hyunwoo.kim>
2022-05-22 10:56:15 +0900

    Create LocalStorage Manager & Update GlobalStore

---
+++ src/store/localStorage.ts
@@ -0,0 +1,57 @@
+const prefix = 'triviaQuiz';
+const property = {
+  startTime: 'startTime',
+  finishTime: 'finishTime',
+  quizIndex: 'quizIndex',
+  correctQuestions: 'correctQuestions',
+  incorrectQuestions: 'incorrectQuestions',
+};
+
+export class LocalStorage {
+  static get startTime(): Date {
+    const time = window.localStorage.getItem(`${prefix}:${property.startTime}`) || '';
+    return new Date(time);
+  }
+  static setStartTime(time: Date) {
+    window.localStorage.setItem(`${prefix}:${property.startTime}`, time.toISOString());
+  }
+
+  static get finishTime(): Date {
+    const time = window.localStorage.getItem(`${prefix}:${property.finishTime}`) || '';
+    return new Date(time);
+  }
+  static setFinishTime(time: Date) {
+    window.localStorage.setItem(`${prefix}:${property.finishTime}`, time.toISOString());
+  }
+
+  static get quizIndex(): number {
+    return Number(window.localStorage.getItem(`${prefix}:${property.quizIndex}`) || -1);
+  }
+  static setQuizIndex(quizIndex: number) {
+    window.localStorage.setItem(`${prefix}:${property.quizIndex}`, String(quizIndex));
+  }
+
+  static get correctQuestions(): number {
+    return Number(window.localStorage.getItem(`${prefix}:${property.correctQuestions}`) || 0);
+  }
+  static setCorrectQuestions(correctQuestions: number) {
+    window.localStorage.setItem(`${prefix}:${property.correctQuestions}`, String(correctQuestions));
+  }
+
+  static get incorrectQuestions(): number {
+    return Number(window.localStorage.getItem(`${prefix}:${property.incorrectQuestions}`) || 0);
+  }
+  static setIncorrectQuestions(incorrectQuestions: number) {
+    window.localStorage.setItem(
+      `${prefix}:${property.incorrectQuestions}`,
+      String(incorrectQuestions)
+    );
+  }
+
+  static init() {
+    window.localStorage.removeItem(`${prefix}:${property.startTime}`);
+    window.localStorage.removeItem(`${prefix}:${property.quizIndex}`);
+    window.localStorage.removeItem(`${prefix}:${property.correctQuestions}`);
+    window.localStorage.removeItem(`${prefix}:${property.incorrectQuestions}`);
+  }
+}
--- src/store/useGlobalStore.ts
+++ src/store/useGlobalStore.ts
@@ -1,25 +1,52 @@
+import { LocalStorage } from '@/store/localStorage';
 import { mountStoreDevtool } from 'simple-zustand-devtools';
 import create from 'zustand';
 
 interface GlobalState {
+  startTime: Date;
+  setStartTime: () => void;
+  finishTime: Date;
+  setFinishTime: () => void;
+  quizIndex: number;
+  setQuizIndex: (quizIndex: number) => void;
   correctQuestions: number;
   setCorrectQuestions: (correctQuestions: number) => void;
   incorrectQuestions: number;
   setIncorrectQuestions: (incorrectQuestions: number) => void;
+  init: () => void;
 }
 
 export const useGlobalStore = create<GlobalState>(set => ({
-  correctQuestions: 0,
-  setCorrectQuestions: (correctQuestions: number) => {
-    set({
-      correctQuestions: correctQuestions,
-    });
+  startTime: LocalStorage.startTime,
+  setStartTime: () => {
+    const startTime = new Date();
+    set({ startTime });
+    LocalStorage.setStartTime(startTime);
   },
-  incorrectQuestions: 0,
+  finishTime: LocalStorage.finishTime,
+  setFinishTime: () => {
+    const finishTime = new Date();
+    set({ finishTime });
+    LocalStorage.setFinishTime(finishTime);
+  },
+  quizIndex: LocalStorage.quizIndex,
+  setQuizIndex: (quizIndex: number) => {
+    set({ quizIndex });
+    LocalStorage.setQuizIndex(quizIndex);
+  },
+  correctQuestions: LocalStorage.correctQuestions,
+  setCorrectQuestions: (correctQuestions: number) => {
+    set({ correctQuestions });
+    LocalStorage.setCorrectQuestions(correctQuestions);
+  },
+  incorrectQuestions: LocalStorage.incorrectQuestions,
   setIncorrectQuestions: (incorrectQuestions: number) => {
-    set({
-      incorrectQuestions: incorrectQuestions,
-    });
+    set({ incorrectQuestions });
+    LocalStorage.setIncorrectQuestions(incorrectQuestions);
+  },
+  init: () => {
+    set({ quizIndex: -1, correctQuestions: 0, incorrectQuestions: 0 });
+    LocalStorage.init();
   },
 }));
 
