f8ee458a97f414b3417bfd2907cc87ad4e12ee37
heptacode <admin@hyunwoo.kim>
2022-05-25 00:24:56 +0900

    Add useResult Exception

--- src/hooks/useResult.ts
+++ src/hooks/useResult.ts
@@ -1,10 +1,17 @@
 import { useGlobalStore } from '@/store/useGlobalStore';
 import { localStorage } from '@/store/useLocalStorage';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export function useResult() {
   const navigate = useNavigate();
   const { init } = useGlobalStore();
+
+  useEffect(() => {
+    if (!localStorage.records) {
+      return navigate('/');
+    }
+  }, []);
 
   function retry() {
     init();
