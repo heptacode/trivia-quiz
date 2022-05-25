e7418279f9257981374cf6d5f7792e3f329bb1a1
heptacode <admin@hyunwoo.kim>
2022-05-25 11:09:12 +0900

    Update useLocalStorage

--- src/store/useLocalStorage.ts
+++ src/store/useLocalStorage.ts
@@ -22,6 +22,8 @@ function get<T = any>(key: string, fallbackValue?: T): T {
   const value = window.localStorage.getItem(`${prefix}:${key}`);
   if (value === null || value === undefined) {
     return fallbackValue as T;
+  } else if (typeof JSON.parse(JSON.stringify(value)) === 'string') {
+    return JSON.parse(JSON.stringify(value)) as T;
   }
 
   return JSON.parse(value) as T;
